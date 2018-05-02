7737c0aeb6aa6473afd4d89de85e6db080cb268c
Jack Grahl <jack.grahl@gmail.com>
2018-05-02 22:28:53 +0000

    Add code for calculating the intersection of two planes.

--- geometry.py
+++ geometry.py
@@ -6,3 +6,14 @@ planes = [
     (2, 1),
     (2, -1),
 ]
+
+
+def plane_intersection(vector, ortho):
+    """
+        x_dimension = value
+        v . ortho = 0
+    """
+    dimension, value = ortho
+    const = 0 - value * vector[dimension]
+    coefficients = vector[:dimension] + vector[dimension:]
+    return coefficients, const
--- test_geometry.py
+++ test_geometry.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 
-from geometry import planes
+from geometry import planes, plane_intersection
 
 
 class TestPlanes(TestCase):
     def test_length(self):
         self.assertEqual(len(planes), 6)
+
+
+class TestPlaneIntersection(TestCase):
+    def test_basic(self):
+        vector_plane = (1, 1, 1)
+        ortho_plane = (2, 2)
+        intersection = plane_intersection(vector_plane, ortho_plane)
