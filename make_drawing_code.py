d7f4161275186958d877c8e7c5c36f7dbb50a5fc
Jack Grahl <jack.grahl@gmail.com>
2018-05-07 23:00:52 +0000

    Add more points with different colors.

--- make_drawing_code.py
+++ make_drawing_code.py
@@ -2,14 +2,19 @@ from drawing import draw_line
 from geometry import plane_drawing
 
 POINTS = [
-    (1, 0, 0)
+    ((1, 0, 0), "green"),
+    ((1, 0, 1), "blue"),
+    ((1, -1, 0), "red"),
+    #((0, -1, -1), "brown"),
 ]
 
 
 def drawing_code(points):
-    for p in points:
+    for p, color in points:
+        print(p)
         for line in plane_drawing(p):
-            c = draw_line(*line)
+            print(line, color)
+            c = draw_line(*line, color=color)
             yield c
 
 
