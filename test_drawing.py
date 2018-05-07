d41d3b937cf7b524157268dc69c901f28a104a94
Jack Grahl <jack.grahl@gmail.com>
2018-05-07 22:09:50 +0000

    Add spacing to code.

--- test_drawing.py
+++ test_drawing.py
@@ -8,9 +8,9 @@ class TestDrawLine(unittest.TestCase):
         x = (0, 1)
         z = (1, 0.5)
         draw_code = draw_line(x, z)
-        self.assertEqual(draw_code, "\draw (0.00,1.00) -- (1.00,0.50);")
+        self.assertEqual(draw_code, "    \draw (0.00,1.00) -- (1.00,0.50);")
         
     def test_basic_line_with_star(self):
         line = [(0, 1), (1, 0.5)]
         draw_code = draw_line(*line)
-        self.assertEqual(draw_code, "\draw (0.00,1.00) -- (1.00,0.50);")
+        self.assertEqual(draw_code, "    \draw (0.00,1.00) -- (1.00,0.50);")
