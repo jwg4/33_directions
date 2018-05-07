b3f2f0c4a2c33a6f0b52a540c7e5fe183108eb35
Jack Grahl <jack.grahl@gmail.com>
2018-05-07 22:02:50 +0000

    Add a test for calling it with a star.

--- drawing.py
+++ drawing.py
@@ -1,6 +1,4 @@
 def draw_line(a, b):
-    print(a)
-    print(b)
     TEMPLATE = "\draw (%.02f,%.02f) -- (%.02f,%.02f);"
     s = TEMPLATE % (a[0], a[1], b[0], b[1])
     return s
--- test_drawing.py
+++ test_drawing.py
@@ -5,6 +5,12 @@ from drawing import draw_line
 
 class TestDrawLine(unittest.TestCase):
     def test_basic_line(self):
+        x = (0, 1)
+        z = (1, 0.5)
+        draw_code = draw_line(x, z)
+        self.assertEqual(draw_code, "\draw (0.00,1.00) -- (1.00,0.50);")
+        
+    def test_basic_line_with_star(self):
         line = [(0, 1), (1, 0.5)]
-        draw_code = draw_line(line[0], line[1])
+        draw_code = draw_line(*line)
         self.assertEqual(draw_code, "\draw (0.00,1.00) -- (1.00,0.50);")
