e7c75855a83b3a5de1074d7516912821632c0f76
Jack Grahl <jack.grahl@gmail.com>
2018-06-07 21:01:07 +0000

    Write to the tex file from the script.

--- make_drawing_code.py
+++ make_drawing_code.py
@@ -11,13 +11,12 @@ POINTS = [
 
 def drawing_code(points):
     for p, color in points:
-        print(p)
         for line in plane_drawing(p):
-            print(line, color)
             c = draw_line(*line, color=color)
             yield c
 
 
 if __name__ == '__main__':
     s = "\n".join(drawing_code(POINTS))
-    print(s)
+    with open("planes.tex", "w") as f:
+        f.write(s)
