71adb17b1bd01e1096c2458b22c9247d0ac38cda
Jack Grahl <jack@prismfp.com>
2018-07-19 16:45:39 +0100

    Try and improve the distinctiveness of the colors.

--- color.py
+++ color.py
@@ -13,9 +13,12 @@ def generate_colors():
     n = len(COLORS)
     for i in range(0, n):
         yield COLORS[i]
-        for j in range(i+1, n):
-            for d in [15, 30, 50, 70, 85]:
+        for j in range(i+1, n-1):
+            for d in [20, 40, 60, 80]:
                 yield "%s!%d!%s" % (COLORS[i], d, COLORS[j])
+        for b in ["white", "black"]:
+            for d in [25, 50, 75]:
+                yield "%s!%d!%s" % (COLORS[i], d, b)
 
 COLOR_LIST = list(generate_colors())
 random.shuffle(COLOR_LIST)
