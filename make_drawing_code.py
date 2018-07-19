23206afb6c735912618be05d4f1844ed6aa4e6b6
Jack Grahl <jack@prismfp.com>
2018-07-19 17:11:02 +0100

    Only print points on a single plane.

--- make_drawing_code.py
+++ make_drawing_code.py
@@ -9,12 +9,12 @@ from points import color_points
 
 def drawing_code(points):
     for p, color in points:
-        for line in plane_drawing(p):
-            c = draw_line(*line, color=color)
-            yield c
+        if p[0] in [1, -1]:
+            for line in plane_drawing(p):
+                c = draw_line(*line, color=color)
+                yield c
         for point in antipodes(p):
             c = draw_point(point, color=color)
-            print(p, point, c)
             yield c
 
 
